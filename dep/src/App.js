108eaef0ee77ebef8daf443a55980d58b88741a5
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2018-09-15 13:28:08 -0700

    click

--- dep/src/App.js
+++ dep/src/App.js
@@ -1,21 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Game from "./components/Game";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <img src="./assets/rickandmorty_ep308_002_Way_Ahead_Of_The_Reveal.jpg"/>
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => <Game />;
 
 export default App;
